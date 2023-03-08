d7aced9826a6c5b552be03a7a03ae50692725ec8
깃Boom0704 <relonging@naver.com>
2023-03-08 12:25:51 +0900

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 10;
+        System.out.println("당신의 나이 : " + a);
+//        if(a > 20){
+//            System.out.println("성인");
+//        }
+        String b = (a>20) ? "성인" : "학생";
+        System.out.println(b);
     }
+
 }
