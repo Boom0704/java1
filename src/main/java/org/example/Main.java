2691129dcf22259456a42bf735fd8f883b5f7dae
깃Boom0704 <relonging@naver.com>
2023-03-13 13:24:39 +0900

    커밋메시지

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,38 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        System.out.println("당신의 나이 : " + a);
-//        if(a > 20){
-//            System.out.println("성인");
-//        }
-        String b = (a>20) ? "성인" : "학생";
-        System.out.println(b);
+        계산기 계산기 = new 계산기(3, 10);
+        System.out.println(계산기.합산());
     }
+}
 
+class 계산기{
+    계산기(){}
+    계산기(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    int a;
+    int b;
+    int 곱(){
+        return this.a * this.b;
+    }
+    int 덧(){
+        return this.a + this.b;
+    }
+    int 뺄(){
+        return this.a - this.b;
+    }
+    int 나(){
+        return this.a / this.b;
+    }
+    int 남(){
+        return this.a % this.b;
+    }
+    int 합산(){
+        int val = 0;
+        for(int i = this.a; i <= this.b; i++)
+            val += i;
+        return val;
+    }
 }
